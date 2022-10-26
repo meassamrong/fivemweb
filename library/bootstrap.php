82079e542fa058e66ef989c663f4aa0c9b728338
meassamrong <meassamrong99@gmail.com>
2022-10-26 19:44:10 +0700

    update

--- library/bootstrap.php
+++ library/bootstrap.php
@@ -20,7 +20,7 @@
 <script src="./boostrap/js/bootstrap.esm.min.js"></script>
 
 <!-- jQuery Plugin -->
-<script src="jquery.3.3.js"></script>
+<script src="library/jquery.3.3.js"></script>
 
 <!-- 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
--- main.js
+++ main.js
@@ -1,8 +1,31 @@
-window.onload = function() {
+
+
+const wait = (delay = 0) =>
+  new Promise(resolve => setTimeout(resolve, delay));
+
+const setVisible = (elementOrSelector, visible) => 
+  (typeof elementOrSelector === 'string'
+    ? document.querySelector(elementOrSelector)
+    : elementOrSelector
+  ).style.display = visible ? 'block' : 'none';
+
+setVisible('.page', false);
+setVisible('#loading', true);
+
+document.addEventListener('DOMContentLoaded', () =>
+  wait(1000).then(() => {
+    setVisible('.page', true);
+    setVisible('#loading', false);
+  }));
+
+
+
+//   mouse crusor
+  $(document).ready(function() {
     document.body.addEventListener("mousemove", function(event) {
         var x = event.pageX - 1 + "px"
         var y = event.pageY + 1 + "px"
         document.getElementById("cursor").style.left = x;
         document.getElementById("cursor").style.top = y;
     })
-};
+})
